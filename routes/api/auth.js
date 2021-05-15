5c2832309d9201d3f7fb4be9a47642f953b598d8
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-05-15 11:11:42 +0530

    updated register path

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,7 @@ const bcrypt = require('bcryptjs');
 // @access  Public
 router.post('/',
     [
-        check('toastmaster_id',"Please include a valid email address").not().isEmpty(),
+        check('toastmaster_id',"Please include an id").not().isEmpty(),
         check('password',"Password is required").exists()
     ],
     async(req,res)=>{
@@ -57,5 +57,65 @@ router.post('/',
         }
     });
 
+    // @route   POST api/auth/users
+// @desc    Register user
+// @access  Public
+router.post('/users',
+[
+    check('name', 'Name is required').not().isEmpty(),
+    check('toastmaster_id',"Please include an id").not().isEmpty(),
+    check('password',"Please enter a password of atleast 6 characters").isLength({min: 6}),
+    check('pathway',"Please include a pathway").not().isEmpty(),
+
+],
+async(req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const {name, toastmaster_id, password, pathway, role, avatar} = req.body;
+
+    try{
+        let user = await User.findOne({toastmaster_id});
+
+        // See if user exists
+        if(user){
+           return res.status(400).json({errors: [{msg:"User already exists"}]});
+        }
+       
+        user = new User({
+            name,
+            toastmaster_id,
+            password,
+            pathway,
+            role,
+            avatar
+        });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt)
+        await user.save();
+
+        // Return jsonwebtoken
+        const payload = {
+            user:{
+                id: user.id,
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),{expiresIn: 360000}, (err,token)=>{
+            if(err){
+                throw err;
+            }
+            res.json({token});
+        });
+
+    }catch(e){
+        console.error(e.message);
+        res.status(500).send('Server Error!')
+    }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,66 +7,7 @@ const config = require('config')
 
 const User = require('../../models/User')
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
-router.post('/',
-    [
-        check('name', 'Name is required').not().isEmpty(),
-        check('toastmaster_id',"Please include an id").not().isEmpty(),
-        check('password',"Please enter a password of atleast 6 characters").isLength({min: 6}),
-        check('pathway',"Please include a pathway").not().isEmpty(),
 
-    ],
-    async(req,res)=>{
-
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
-        }
-        const {name, toastmaster_id, password, pathway, role, avatar} = req.body;
-
-        try{
-            let user = await User.findOne({toastmaster_id});
-
-            // See if user exists
-            if(user){
-               return res.status(400).json({errors: [{msg:"User already exists"}]});
-            }
-           
-            user = new User({
-                name,
-                toastmaster_id,
-                password,
-                pathway,
-                role,
-                avatar
-            });
-
-            // Encrypt password
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt)
-            await user.save();
-
-            // Return jsonwebtoken
-            const payload = {
-                user:{
-                    id: user.id,
-                }
-            }
-
-            jwt.sign(payload, config.get('jwtSecret'),{expiresIn: 360000}, (err,token)=>{
-                if(err){
-                    throw err;
-                }
-                res.json({token});
-            });
-
-        }catch(e){
-            console.error(e.message);
-            res.status(500).send('Server Error!')
-        }
-    });
 
 
 module.exports = router;
