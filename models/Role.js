a5b26802d71c6f27002b3412942b21752a9dbf41
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-05-15 09:11:34 +0530

    Create Role.js

---
+++ models/Role.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const RoleSchema = new mongoose.Schema({
+    role:{
+        type:String,
+        required: true
+    },
+    meeting_id:{
+        type: Number,
+        required: true
+    },
+    date:{
+        type:Date,
+        required: true
+    },
+    commendation:{
+        type: String,
+    }
+})
