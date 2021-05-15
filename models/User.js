dcd7004e09ce4383554edc705422539aa265a460
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-05-15 09:06:24 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    pathway:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        default: member
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema); 
